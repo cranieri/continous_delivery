9d38dc63c5b4606b85ccbc43e219fedf36b51f9a
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2016-12-30 11:16:25 +0000

    changes

--- terraform/deploy-cd.rb
+++ terraform/deploy-cd.rb
@@ -7,7 +7,5 @@
 # sudo terraform apply
 
 ##Run Ansible playbooks
-echo "Quick sleep while instances spin up"
-sleep 20
 echo "Ansible provisioning"
 ansible-playbook ../ansible/playbooks/main-cd.yml
