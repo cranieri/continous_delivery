124a99fc682e125f04b21fb97d086d1a4eef8b56
Cosimo Ranieri <cosimo.ranieri@currencycloud.com>
2016-12-30 10:11:31 +0000

    provision cd machine

---
+++ terraform/deploy-cd.rb
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##Create infrastructure and inventory file
+echo "Creating infrastructure"
+sudo rm -rf .terraform/
+sudo terraform get -update
+sudo terraform apply
+
+##Run Ansible playbooks
+echo "Quick sleep while instances spin up"
+sleep 20
+echo "Ansible provisioning"
+ansible-playbook ../ansible/playbooks/main-cd.yml
